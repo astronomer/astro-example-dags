c4f3ff3ffe2ce13d61077563911ceb0ddb6ac4e3
sachintungaria <122982321+sachintungaria@users.noreply.github.com>
2024-01-31 18:20:09 +0530

    Update samplehelloworld.py

--- dags/samplehelloworld.py
+++ dags/samplehelloworld.py
@@ -1,3 +1,23 @@
 from datetime import datetime , timedelta
 from airflow.utils.dates import days_ago
 
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+defalu_args={
+  'owner'= 'Sachin Tungaria'
+}
+dag= DAG(
+  dag_id='hello world',
+  description='our first "hello world" DAG!',
+  default_args=default_args,
+  start_date=days_ago(1),
+  schedule_interval=1
+)
+task= BashOpeartor(
+  task_id='hello world task',
+  bash_command='echo hello world!',
+  dag=dag
+)
+
+task
