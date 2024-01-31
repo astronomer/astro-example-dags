2dabcfa24104dea2316cbf896be98d294ca6c9ed
sachintungaria <122982321+sachintungaria@users.noreply.github.com>
2024-01-31 18:39:29 +0530

    Update samplehelloworld.py

--- dags/samplehelloworld.py
+++ dags/samplehelloworld.py
@@ -1,23 +1,26 @@
-from datetime import datetime , timedelta
+from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator  # Correct import
 
-defalu_args={
-  'owner'= 'Sachin Tungaria'
+default_args = {
+    'owner': 'airflow'
 }
-dag= DAG(
-  dag_id='hello world',
-  description='our first "hello world" DAG!',
-  default_args=default_args,
-  start_date=days_ago(1),
-  schedule_interval=1
-)
-task= BashOpeartor(
-  task_id='hello world task',
-  bash_command='echo hello world!',
-  dag=dag
+
+dag = DAG(
+    dag_id='hello_world',  # Changed dag_id to use underscores instead of spaces
+    description='our first "hello world" DAG!',
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval='@once'  # Changed schedule_interval to a valid value
 )
 
-task
+task = BashOperator(
+    task_id='hello_world_task',  # Changed task_id to use underscores instead of spaces
+    bash_command='echo hello world!',
+    dag=dag
+)
+
+task  # This line is not needed
+
