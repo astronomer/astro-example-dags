c8475121aa5b4844106ad5d2039783541fae55af
Martin Moss <martyzz1@gmail.com>
2024-05-15 10:16:32 +0100

    Make Get_columns work for both view, tables and Materialized views

--- plugins/operators/mixins/get_columns_from_table.py
+++ plugins/operators/mixins/get_columns_from_table.py
@@ -25,12 +25,33 @@ class GetColumnsFromTableMixin:
             WHERE table_schema = '{schema_name}' AND table_name = '{table_name}'
             ORDER BY ordinal_position;
         """
+
+        materialized_sql = f"""
+            SELECT attname AS column_name,
+                format_type(atttypid, atttypmod) AS data_type,
+                attnotnull AS is_not_nullable
+            FROM pg_attribute
+            JOIN pg_class c ON c.oid = pg_attribute.attrelid
+            JOIN pg_namespace n ON n.oid = c.relnamespace
+            WHERE c.relname = '{table_name}'  -- Replace with your materialized view name
+            AND c.relkind = 'm'  -- 'm' indicates a materialized view
+            AND n.nspname = '{schema_name}'
+            AND attnum > 0
+            AND NOT attisdropped;
+
+        """
         self.log.info(f"get_columns_from_table: {sql}")
 
         print("GET COLUMNS", sql)
         # Execute the query and fetch results using SQLAlchemy
         result = conn.execute(sql)
         column_names = [f'{row["column_name"]}' for row in result]
+        if not column_names:
+            self.log.info("No columns returned from information_catalog query, assuming its a materialized view")
+            self.log.info(f"get_columns_from_table: {materialized_sql}")
+            result = conn.execute(materialized_sql)
+            column_names = [f'{row["column_name"]}' for row in result]
+
         if not column_names:
             raise AirflowException(f"No columns found for table {schema_name}.{table_name}")
 
