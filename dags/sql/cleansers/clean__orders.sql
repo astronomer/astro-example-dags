e3d165bfeff7128030e755804726e30d43c59da9
Chloe Murphy <csgmurphy@gmail.com>
2024-06-04 18:13:39 +0100

    cleaned version

--- dags/sql/cleansers/clean__orders.sql
+++ dags/sql/cleansers/clean__orders.sql
@@ -37,11 +37,7 @@ linked_sd AS (
 first_order AS (
     SELECT
         customer_id,
-<<<<<<< Updated upstream
         MIN(DATE(createdat)) AS first_order_date
-=======
-        MIN(DATE(createdat)) AS first_order_datest
->>>>>>> Stashed changes
     FROM
         public.orders
     GROUP BY
