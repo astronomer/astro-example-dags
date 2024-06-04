3d3da1599806ef57633654ea646a3078902a6349
Chloe Murphy <csgmurphy@gmail.com>
2024-06-04 15:08:24 +0100

    Add clean__orders.sql

---
+++ dags/sql/cleansers/clean__orders.sql
@@ -0,0 +1,64 @@
+-- CLEANED ORDERS
+-- Including first customer order, true customer type, and linked ship direct
+
+-- Extract ship_direct orders
+WITH ship_directs AS (
+    SELECT
+        id,
+        order_name,
+        createdat,
+        initiated_sale__original_order_id
+    FROM
+        public.orders
+    WHERE
+        order_type = 'ship_direct'
+        AND order_name != ' '
+),
+
+-- Link ship_direct orders with their original orders
+linked_sd AS (
+    SELECT
+        sd.id,
+        sd.order_name,
+        sd.createdat,
+        sd.initiated_sale__original_order_id,
+        o.order_name AS original_order_name_sd
+    FROM
+        ship_directs sd
+    LEFT JOIN
+        public.orders o
+    ON
+        sd.initiated_sale__original_order_id = o.id
+    WHERE
+        o.link_order__is_child != 1
+),
+
+-- Determine the first order date for each customer
+first_order AS (
+    SELECT
+        customer_id,
+        MIN(DATE(createdat)) AS first_order_date
+    FROM
+        public.orders
+    GROUP BY
+        customer_id
+)
+
+SELECT
+    o.*,  -- Include all columns from orders
+    fo.first_order_date,
+    CASE
+        WHEN fo.first_order_date = DATE(o.createdat) THEN 'New Customer'
+        ELSE 'Returning Customer'
+    END AS customer_type_,
+    lsd.original_order_name_sd
+FROM
+    public.orders o
+LEFT JOIN
+    linked_sd lsd
+ON
+    lsd.id = o.id
+LEFT JOIN
+    first_order fo
+ON
+    fo.customer_id = o.customer_id;
