b6f96e3d9388b90d3ba3aaaa2cb26ae408eeb6b4
Chloe Murphy <csgmurphy@gmail.com>
2024-06-05 10:42:50 +0100

    add ship direct orders

---
+++ dags/sql/reports/rep__ship__direct_orders.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS {{ schema }}.ship_direct_orders CASCADE;
+CREATE VIEW {{ schema }}.ship_direct_orders AS
+SELECT b.order_name AS original_order_name, a.*
+    FROM (
+        SELECT *
+        FROM public.clean__order__summary
+        WHERE
+        order_type = 'ship_direct') a
+    LEFT JOIN public.clean__order__summary b
+    ON a.initiated_sale__original_order_id = b.id
+    WHERE a.order_status != 'cancelled'
+    AND b.link_order__is_child != 1;
