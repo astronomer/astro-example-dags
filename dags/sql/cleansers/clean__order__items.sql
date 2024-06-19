bfc989730cd10d662087f0e7edbbc2ce616cb3fc
Chloe Murphy <csgmurphy@gmail.com>
2024-06-19 14:37:13 +0100

    adding inspire me flag and commission perc

--- dags/sql/cleansers/clean__order__items.sql
+++ dags/sql/cleansers/clean__order__items.sql
@@ -6,12 +6,14 @@ CREATE VIEW {{ schema }}.clean__order__items AS
 	oi.original_order_name AS partner_order_name,
 	oi.order_name AS harper_order_name,
 	oi.original_name AS product_name,
+    CASE WHEN  order_type = 'inspire_me' THEN 1 ELSE 0 END AS is_inspire_me,
 	oi.price as item_price_pence,
 	oi.discount AS item_discount_price_pence,
 	oi.price - oi.discount AS item_value_pence,
     oi.calculated_discount AS calculated_item_discount_price_pence,
 	oi.price - oi.calculated_discount AS calculated_item_value_pence,
 	oi.qty AS item_quantity,
+<<<<<<< Updated upstream
     /*CASE WHEN oi.commission__amount THEN oi.commission__amount
     ELSE
     CASE WHEN oi.commission__percentage IS NOT NULL THEN
@@ -20,6 +22,13 @@ CREATE VIEW {{ schema }}.clean__order__items AS
         NULL
        END
     END AS commission__calculated_amount,*/
+=======
+	CASE WHEN oi.commission__percentage IS NOT NULL THEN
+	    oi.price * ( oi.commission__percentage / 100)
+	    ELSE
+	        NULL
+    END AS commission__calculated_amount,
+>>>>>>> Stashed changes
 	 {{ dim__time_columns | prefix_columns('oc', 'createdat') }}
 FROM {{ schema }}.order__items oi
 LEFT JOIN
