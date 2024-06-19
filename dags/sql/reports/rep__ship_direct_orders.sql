6077d6fc647e58e5ead44af8cd1ab5a178b4ea18
Chloe Murphy <csgmurphy@gmail.com>
2024-06-19 10:59:59 +0100

    change source table & order reference name

--- dags/sql/reports/rep__ship_direct_orders.sql
+++ dags/sql/reports/rep__ship_direct_orders.sql
@@ -1,12 +1,12 @@
 DROP VIEW IF EXISTS {{ schema }}.rep__ship_direct_orders CASCADE;
 CREATE VIEW {{ schema }}.rep__ship_direct_orders AS
-SELECT b.order_name AS original_order_name, a.*
+SELECT b.order_name AS previous_order_name, a.*
     FROM (
         SELECT *
-        FROM {{ schema }}.rep__order__summary.sql
+        FROM {{ schema }}.clean__order__summary
         WHERE
         order_type = 'ship_direct') a
-    LEFT JOIN public.clean__order__summary b
+    LEFT JOIN {{ schema }}.clean__order__summary b
     ON a.initiated_sale__original_order_id = b.id
     WHERE a.order_status != 'cancelled'
-    AND b.link_order__is_child != 1;
+    AND b.link_order__is_child != 1
