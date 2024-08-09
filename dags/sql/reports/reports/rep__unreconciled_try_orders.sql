b4d5c64946917e18e68a90527bcf7d67d48f2920
Martin Moss <martyzz1@gmail.com>
2024-08-09 02:56:09 +0100

    Add the needs_invoicing check to the unreconciled orders report

--- dags/sql/reports/reports/rep__unreconciled_try_orders.sql
+++ dags/sql/reports/reports/rep__unreconciled_try_orders.sql
@@ -5,7 +5,11 @@ DROP MATERIALIZED VIEW IF EXISTS {{ schema }}.rep__unreconciled_try_orders CASCA
 CREATE MATERIALIZED VIEW IF NOT EXISTS {{ schema }}.rep__unreconciled_try_orders AS
 
     SELECT
-        o.id,
+        CASE
+          WHEN o.itemsummary__num_received_by_partner_warehouse < o.itemsummary__total_items
+            THEN 'true'
+            ELSE 'false'
+        END AS needs_invoicing,
         o.order_name,
         o.order_status,
         o.brand_name,
@@ -28,7 +32,8 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS {{ schema }}.rep__unreconciled_try_orders
         o.trial_period_actually_ended_at,
         o.orderstatusevent__trialperiodended_at,
         o.orderstatusevent__trialperiodfinalreconciliation_at,
-        o.trial_period_actually_reconciled_at
+        o.trial_period_actually_reconciled_at,
+        o.id
     FROM {{ schema }}.rep__order__summary o
     LEFT JOIN clean__partners p ON p.id = o.partner_id
 
@@ -38,7 +43,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS {{ schema }}.rep__unreconciled_try_orders
         AND o.fulfillment_status NOT IN ('fulfilled', 'unfulfilled')
         AND o.order_status NOT IN ('cancelled', 'in_try_on_period') AND order_type = 'harper_try'
         AND o.itemsummary__num_received_by_partner_warehouse < itemsummary__total_items
-    ORDER BY o.brand_name, o.createdat ASC
+    ORDER BY needs_invoicing DESC, o.brand_name, o.createdat ASC
 WITH NO DATA;
 
 {% if is_modified %}
