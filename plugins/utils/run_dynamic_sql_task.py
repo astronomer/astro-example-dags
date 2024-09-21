46a564d3b2e12b013b1ecea6e200f34cc53b6a38
Martin Moss <martyzz1@gmail.com>
2024-09-21 17:50:46 +0100

    [main] Add short circuit operator into the SQL tasks.

--- plugins/utils/run_dynamic_sql_task.py
+++ plugins/utils/run_dynamic_sql_task.py
@@ -1,7 +1,9 @@
 import os
 
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import ShortCircuitOperator
 
+from plugins.utils.is_latest_active_dagrun import is_latest_dagrun
 from plugins.utils.get_recursive_sql_file_lists import get_recursive_sql_file_lists
 from plugins.utils.send_harper_slack_notification import send_harper_failure_notification
 
@@ -21,13 +23,21 @@ def run_dynamic_sql_task(
     exported_schemas_path = "../../include/exportedSchemas/"
     exported_schemas_abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)), exported_schemas_path)
 
+    is_latest_dagrun_task = ShortCircuitOperator(
+        task_id="skip_check",
+        python_callable=is_latest_dagrun,
+        depends_on_past=False,
+        dag=dag,
+    )
+
     sql_files = get_recursive_sql_file_lists(
         sql_dir_abspath,
         subdir=sql_type,
         add_table_columns_to_context=add_table_columns_to_context,
         check_entity_pattern=check_entity_pattern,
     )
-    last_task = wait_for_task
+    wait_for_task >> is_latest_dagrun_task
+    last_task = is_latest_dagrun_task
 
     for group_index, group_list in enumerate(sql_files, start=1):
         task = DummyOperator(task_id=f"{sql_type}_{group_index}", dag=dag)
