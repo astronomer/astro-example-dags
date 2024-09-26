0f5f79da930e9ed6c497691c82ab51e362189829
PK321 <52413049+PK321@users.noreply.github.com>
2024-09-26 15:30:51 +0530

    Create mydag1.py

---
+++ dags/mydag1.py
@@ -0,0 +1,22 @@
+"""
+this is for testing
+"""
+
+from airflow import Dataset
+from airflow.decorators import dag, task
+from pendulum import datetime
+import requests
+
+#Define the basic parameters of the DAG, like schedule and start_date
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    doc_md=__doc__,
+    default_args={"owner": "Astro", "retries": 3},
+    tags=["example"],
+)
+start = print("I am in start")
+end = print("i am in end")
+
+start >> end
