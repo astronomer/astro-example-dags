b24887eca11f2b55e52dbe48099b3f20ee3339ae
Chloe Murphy <csgmurphy@gmail.com>
2024-10-15 16:46:51 +0100

    removing penc to pound conversion

--- dags/sql/cleansers/cleansers/clean__order__item__summary.sql
+++ dags/sql/cleansers/cleansers/clean__order__item__summary.sql
@@ -16,30 +16,30 @@ CREATE VIEW {{ schema }}.clean__order__item__summary AS
         SUM(oi.return_requested_by_customer) AS num_return_requested_by_customer,
         SUM(oi.return_sent_by_customer) AS num_return_sent_by_customer,
 		-- value summary
-		SUM(oi.calculated_item_value_pence)/100 AS total_value_ordered,
-		SUM(CASE WHEN oi.purchased = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS total_value_purchased,
-		SUM(CASE WHEN oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS total_value_returned,
-		SUM(CASE WHEN oi.received = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS total_value_received,
+		SUM(oi.calculated_item_value_pence) AS total_value_ordered,
+		SUM(CASE WHEN oi.purchased = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS total_value_purchased,
+		SUM(CASE WHEN oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS total_value_returned,
+		SUM(CASE WHEN oi.received = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS total_value_received,
         (SUM(CASE WHEN oi.purchased = 1 THEN oi.calculated_item_value_pence ELSE 0 END)
-        - SUM(CASE WHEN oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END))/100 AS total_value_purchased_net,
-		SUM(CASE WHEN oi.received_by_warehouse = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS total_value_received_by_warehouse,
+        - SUM(CASE WHEN oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END)) AS total_value_purchased_net,
+		SUM(CASE WHEN oi.received_by_warehouse = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS total_value_received_by_warehouse,
 		-- item summary for initiated sale
-		COUNT(DISTINCT CASE WHEN (oi.is_initiated_sale = 1) THEN oi.id ELSE NULL END)/100 AS initiated_sale__num_ordered,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.fulfilled ELSE 0 END)/100 AS initiated_sale__num_items_fulfilled,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.purchased ELSE 0 END)/100 AS initiated_sale__num_purchased,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.returned ELSE 0 END)/100 AS initiated_sale__num_returned,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.purchased ELSE 0 END)/100 AS initiated_sale__num_actually_purchased,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.preorder ELSE 0 END)/100 AS initiated_sale__num_preorder,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.received ELSE 0 END)/100 AS initiated_sale__num_received_by_harper_warehouse,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.received_by_warehouse ELSE 0 END)/100 AS initiated_sale__num_received_by_partner_warehouse,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.return_requested_by_customer ELSE 0 END)/100 AS initiated_sale__num_return_requested_by_customer,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.return_sent_by_customer ELSE 0 END)/100 AS initiated_sale__num_return_sent_by_customer,
+		COUNT(DISTINCT CASE WHEN (oi.is_initiated_sale = 1) THEN oi.id ELSE NULL END) AS initiated_sale__num_ordered,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.fulfilled ELSE 0 END) AS initiated_sale__num_items_fulfilled,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.purchased ELSE 0 END) AS initiated_sale__num_purchased,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.returned ELSE 0 END) AS initiated_sale__num_returned,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.purchased ELSE 0 END) AS initiated_sale__num_actually_purchased,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.preorder ELSE 0 END) AS initiated_sale__num_preorder,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.received ELSE 0 END) AS initiated_sale__num_received_by_harper_warehouse,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.received_by_warehouse ELSE 0 END) AS initiated_sale__num_received_by_partner_warehouse,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.return_requested_by_customer ELSE 0 END) AS initiated_sale__num_return_requested_by_customer,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.return_sent_by_customer ELSE 0 END) AS initiated_sale__num_return_sent_by_customer,
 		-- value summary for initiated sale
-		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS initiated_sale__total_value_ordered,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.purchased = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS initiated_sale__total_value_purchased,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS initiated_sale__total_value_returned,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.received = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS initiated_sale__total_value_received,
-		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.received_by_warehouse = 1 THEN oi.calculated_item_value_pence ELSE 0 END)/100 AS initiated__total_value_received_wh, --renamed since too long
+		SUM(CASE WHEN oi.is_initiated_sale = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS initiated_sale__total_value_ordered,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.purchased = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS initiated_sale__total_value_purchased,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.returned = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS initiated_sale__total_value_returned,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.received = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS initiated_sale__total_value_received,
+		SUM(CASE WHEN oi.is_initiated_sale = 1 AND oi.received_by_warehouse = 1 THEN oi.calculated_item_value_pence ELSE 0 END) AS initiated__total_value_received_wh, --renamed since too long
         array_agg(DISTINCT(oi.tracking_url)) AS delivery_tracking_urls
     FROM
         {{ schema }}.orders o
